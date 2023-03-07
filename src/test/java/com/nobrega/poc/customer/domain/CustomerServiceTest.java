10a32f160a7d1ce4ad3c02b8c9b5da36fe169385
Joao Victor Nobrega <joao.nobrega@fanduel.com>
2023-03-07 11:41:01 +0000

    create new constants

--- src/test/java/com/nobrega/poc/customer/domain/CustomerServiceTest.java
+++ src/test/java/com/nobrega/poc/customer/domain/CustomerServiceTest.java
@@ -17,6 +17,8 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 class CustomerServiceTest {
 
+    private static final String NAME = "name";
+    private static final String DOCUMENT = "document";
     @InjectMocks
     private CustomerService service;
     @Mock
@@ -28,13 +30,13 @@ class CustomerServiceTest {
     void testGetCustomerByUuid() {
         String uuid = UUID.randomUUID().toString();
         Customer customer = Customer.builder()
-                .name("name")
-                .document("document")
+                .name(NAME)
+                .document(DOCUMENT)
                 .build();
         CustomerResponse customerResponse = CustomerResponse.builder()
                 .uuid(uuid)
-                .name("name")
-                .document("document")
+                .name(NAME)
+                .document(DOCUMENT)
                 .build();
 
         when(customerRepository.findByUuid(uuid)).thenReturn(Optional.of(customer));
@@ -45,8 +47,8 @@ class CustomerServiceTest {
         verify(customerRepository).findByUuid(uuid);
         verify(customerMapper).convertFromModel(customer);
         assertEquals(uuid, actual.getUuid());
-        assertEquals("name", actual.getName());
-        assertEquals("document", actual.getDocument());
+        assertEquals(NAME, actual.getName());
+        assertEquals(DOCUMENT, actual.getDocument());
 
     }
 }
